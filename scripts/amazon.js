73ba2796d9fc5bac54f2f42814b7e4afd5a321ba
choa1 <choaibhamoud@gmail.com>
2025-10-03 10:43:44 +0100

    make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,28 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating:{
-    stars: 4.5,
-    count: 87
-  },
-  price:1090
-},{
-image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating:{
-    stars: 4.0,
-    count: 127
-  },
-  price:2095
-},{
-image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating:{
-    stars: 4.5,
-    count: 56
-  },
-  price: 799
-}];
 let productsHTML='';
 products.forEach((product) => {
   productsHTML += `
@@ -43,11 +18,9 @@ products.forEach((product) => {
               ${product.rating.count}
             </div>
           </div>
-
           <div class="product-price">
-            ${(product.price/100).toFixed(2) }
+            ${(product.priceCents/100).toFixed(2)}
           </div>
-
           <div class="product-quantity-container">
             <select>
               <option selected value="1">1</option>
@@ -62,17 +35,35 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
-
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
             <img src="images/icons/checkmark.png">
             Added
           </div>
-
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}">
             Add to Cart
           </button>
         </div>`;
-  document.querySelector('.js-products-grid').innerHTML= productsHTML;
 })
+  document.querySelector('.js-products-grid').innerHTML= productsHTML;
+  document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+    const productid = button.dataset.productid;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (item.productid === productid)
+      {
+        matchingItem = item;
+      }
+    })  
+    if (matchingItem)
+    {
+      matchingItem.quantity++;
+    }else {
+      cart.push({
+      productid: productid,
+      quantity: 1
+    });}
+    })
+  })
