6a0f8a53c27308d0323a6937a00ab48308a23b8b
choa1 <choaibhamoud@gmail.com>
2025-10-05 14:36:56 +0100

    use utils and fix price formating and radio opyions

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,10 @@
 export const cart = [{
-  id :"e43638ce-6aa0-4b85-b27f-e1d07eb678c6"
+  id :"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity:1
 },
 {
-  id: "15b6fc6f-327a-4ec4-896f-486349e85a3d"
+  id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity:2
 }];
 export function addToCart(productid){
      let matchingItem;
